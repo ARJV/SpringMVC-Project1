4baff43e30dd06140e96c9cdb81410334e055b59
ARJV <yashnovag@gmail.com>
2024-08-14 23:09:32 +0300

    Реализовал отображение, добавление, редактирование и удаление людей. БЕЗ КНИГ! В DAO в show() изменил порядок аргументов, т.к. предыдущая версия устарела

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import ru.alishev.springcourse.models.Person;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDAO {
@@ -18,5 +19,29 @@ public class PersonDAO {
     public List<Person> index() {
         return jdbcTemplate.query("SELECT * FROM Person",
                 new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person(name, birth_date) VALUES (?, ?)",
+                person.getName(), person.getBirth_date());
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?",
+                new BeanPropertyRowMapper<>(Person.class), id).stream().findAny().orElse(null);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+
+    public void update(int id, Person person) {
+        jdbcTemplate.update("UPDATE Person SET name=?, birth_date=? WHERE id=?",
+                person.getName(), person.getBirth_date(), id);
+    }
+
+    public Optional<Person> show(String name) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE name=?",
+                new BeanPropertyRowMapper<>(Person.class), name).stream().findAny();
     }
 }
